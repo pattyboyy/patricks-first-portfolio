361f27981eb2eb0e707c70a8c6169f911616326a
Patrick Riedinger <patried7@gmail.com>
2024-03-14 16:09:41 -0600

    added js code to allow users to jump to different sections. added an 'about me section'

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// this code should take the user different sections of the portfolio
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
